d1e1ad1f2cde1745b81b0c8963da97ea3e9b88f2
thesojourner <brad@sycamoreleaf.com>
2014-05-09 14:23:21 -0500

    Readme added
    Added Readme and example code

---
+++ readme.js
@@ -0,0 +1,17 @@
+Project YUKON: Alpha is meant to be a project designed to be inclusive and education.  We are a small team of brogrammers working together 
+
+statement(delimiter)(function(){
+
+	var = var;
+	http = 400px;
+	html = 500px;;
+
+	if() { 
+	//supercode
+	} else {
+	//othersupercode
+	}elseif{
+	//moresupercode
+	};
+	
+)};
